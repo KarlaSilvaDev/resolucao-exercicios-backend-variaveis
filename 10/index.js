a85baaf01349f2eaea509a8d2011db394dc19cd0
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-04 20:52:51 -0300

    Ex10 - Cálculo de Delta

---
+++ 10/index.js
@@ -0,0 +1,9 @@
+console.log('--- Cálculo de Delta ---');
+console.log('Equação do segundo grau: ax² + bx + c = 0');
+console.log('Delta = b² - 4ac');
+let a = 5;
+let b = 5;
+let c = 1;
+let delta = b ** 2 - 4 * a * c;
+console.log(`\nDados: \nEquação: ${a}x² + ${b}x + ${c} = 0`);
+console.log(`\nResultado: \nDelta = ${b}² - 4 * ${a} * ${c} = ${delta}`);
