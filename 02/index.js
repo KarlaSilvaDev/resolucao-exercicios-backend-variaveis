a945aa2acb666d0ffc05dec0a7c6eb452d017f0b
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-04 19:43:51 -0300

    Correção do código

--- 02/index.js
+++ 02/index.js
@@ -1,3 +1,3 @@
 let fahrenheit = 73; 
-let celsius = (fahrenheit - 32)*5/9
-console.log(`${fahrenheit}°F é igual a aproximadamente ${celsius.toFixed(0)}°C`)
+let celsius = (fahrenheit - 32)*5/9;
+console.log(`${fahrenheit}°F é igual a aproximadamente ${celsius.toFixed(0)}°C`);
