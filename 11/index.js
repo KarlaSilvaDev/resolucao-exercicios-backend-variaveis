70e25681979155fad49ec47744c2a209e39cdfb1
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-04 20:59:32 -0300

    Ex11 - Volume de uma Esfera

---
+++ 11/index.js
@@ -0,0 +1,8 @@
+console.log('--- Volume de uma Esfera ---');
+console.log('v = (4/3)* pi * r³');
+let diametro = 6;
+let volume = (4/3) * (diametro/2) ** 3;
+console.log(`\nDado: \nDiâmetro (d = 2r): ${diametro}cm`);
+console.log(`\nResultado: \nO volume de uma esfera de raio ${diametro/2}cm é igual a ${volume}PI cm³ `)
+
+
