9f7190f25b4e3932325443ce10dbec69b5aa2122
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-04 20:15:50 -0300

    Ex06 - Cálculo da Velocidade Média

---
+++ 06/index.js
@@ -0,0 +1,7 @@
+console.log('---Cálculo da Velocidade Média---');
+console.log('Velocidade = distPercorrida / tempo');
+let distPercorrida = 500;
+let tempo = 10;
+let velocidade = 3.6 * (distPercorrida / tempo);
+console.log(`\nDados: \nDistância percorrida (m): ${distPercorrida} \nTempo (s): ${tempo}\n`);
+console.log(`Resultado: \nVelocidade média (km/h): ${velocidade}`);
