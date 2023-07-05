6804b0f4f497c4ad2d47523396a728838e38168a
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-05 14:52:45 -0300

    Alteração do nome das variáveis de acordo com o README

--- 13/index.js
+++ 13/index.js
@@ -1,8 +1,8 @@
 console.log('--- Área total de um Cilindro ---');
 console.log('At = 2*pi*R*(R+H)');
-let raioBase = 3;
-let altura = 1;
-let areaTotal = 2 * Math.PI * raioBase * (raioBase + altura);
-console.log(`\nDados: \nRaio da base (R): ${raioBase}cm \nAltura (H): ${altura}cm `);
-console.log(`\nResultado: \nÁrea total (At): ${areaTotal.toFixed(2)}cm² `);
+let r = 3;
+let h = 1;
+let at = 2 * Math.PI * r * (r + h);
+console.log(`\nDados: \nRaio da base (R): ${r}cm \nAltura (H): ${h}cm `);
+console.log(`\nResultado: \nÁrea total (At): ${at.toFixed(2)}cm² `);
 
