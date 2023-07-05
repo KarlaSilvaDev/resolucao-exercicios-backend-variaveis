9d588be8033e928b4be2fcf456aa524581eff993
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-05 14:51:26 -0300

    Alteração do nome das variáveis de acordo com o README

--- 12/index.js
+++ 12/index.js
@@ -1,8 +1,7 @@
 console.log('--- Taxa de Juros ---');
 console.log(' i = (M/C)^(1/n) - 1');
-let capitalInicial = 60_000;
-let numMeses = 24;
-let montante = 90_000;
-let taxaJurosPercentual = ((montante/capitalInicial)**(1/numMeses) - 1)*100;
-console.log(taxaJurosPercentual);
-console.log(`O seu financiamento de R$${capitalInicial.toFixed(2)} teve uma taxa de juros de ${taxaJurosPercentual.toFixed(3)}%, pois após ${numMeses} meses você teve que pagar R$${montante.toFixed(2)}.`);
+let c = 60_000;
+let n = 24;
+let m = 90_000;
+let i = ((m/c)**(1/n) - 1)*100;
+console.log(`O seu financiamento de R$${c.toFixed(2)} teve uma taxa de juros de ${i.toFixed(3)}%, pois após ${n} meses você teve que pagar R$${m.toFixed(2)}.`);
