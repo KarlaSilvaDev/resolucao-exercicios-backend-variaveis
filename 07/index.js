8716f56473f802195112d34e76a5e591419493d7
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-05 14:43:14 -0300

    Alteração do nome das variáveis de acordo com o README

--- 07/index.js
+++ 07/index.js
@@ -1,10 +1,10 @@
 console.log('--- Casos de COVID-19 ---');
 console.log('P = P0 * x^(t/7)')
-let populacaoInicialInfectada = 1_000;
-let potencialTransmissaoPorInfectado = 4;
-let tempoDias = 100;
+let p0 = 1_000;
+let x = 4;
+let t = 100;
 console.log(`\nDados: 
-\nPopulação Inicial Infectada (P0): ${populacaoInicialInfectada} pessoas 
-\nQuantidade de pessoas para as quais um paciente infectado pode transmitir (x): ${potencialTransmissaoPorInfectado} \nTempo (t): ${tempoDias} dias`);
-let populacaoFuturaInfectada=populacaoInicialInfectada*potencialTransmissaoPorInfectado**(tempoDias/7);
-console.log(`\nResultado: \nPopulação futura infectada (P): ${populacaoFuturaInfectada.toFixed(0)} pessoas`);
+\nPopulação Inicial Infectada (P0): ${p0} pessoas 
+\nQuantidade de pessoas para as quais um paciente infectado pode transmitir (x): ${x} \nTempo (t): ${t} dias`);
+let p=p0*x**(t/7);
+console.log(`\nResultado: \nPopulação futura infectada (P): ${p.toFixed(0)} pessoas`);
