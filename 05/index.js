9d121e89c8cb8b81f29babff75d1e6349335ad0a
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-05 14:38:15 -0300

    Alteração do nome das variáveis de acordo com o README

--- 05/index.js
+++ 05/index.js
@@ -4,6 +4,6 @@ x1 = x2 = y1 = 1;
 let y2 = 4;
 console.log('---Cálculo da distância entre dois pontos---');
 console.log('d = ((x2 - x1)² + (y2 -y1)²)^(1/2)')
-let distancia = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
+let d = Math.sqrt((x2 - x1)**2 + (y2 - y1)**2);
 console.log(`Dados: \nx1 = ${x1}cm \nx2 = ${x2}cm \ny1 = ${y1}cm \ny2 = ${y2}cm \n`);
-console.log(`Distância (d) = ${distancia}cm`);
+console.log(`Distância (d) = ${d}cm`);
