d5b3a555e23353273b16aa6cd3ed024f174b0b62
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-05 14:37:06 -0300

    Alteração do nome das variáveis de acordo com o README

--- 04/index.js
+++ 04/index.js
@@ -1,7 +1,7 @@
-let capital = 1_000;
-let tempoMeses = 5;
-let taxaMensal = 12.5;
-let montante = capital*(1 + taxaMensal/100)**tempoMeses;
+let c = 1_000;
+let t = 5;
+let i = 12.5;
+let m = c*(1 + i/100)**t;
 console.log('---Cálculo de Juros Compostos---');
 console.log('M = C(1+i)^t');
-console.log(`Capital (C): R$ ${capital.toFixed(2)} \nTaxa fixa (i): ${taxaMensal}/mês \nTempo (t):${tempoMeses} meses\nMontante (M): R$ ${montante.toFixed(2)}`);
+console.log(`Capital (C): R$ ${c.toFixed(2)} \nTaxa fixa (i): ${i}/mês \nTempo (t):${t} meses\nMontante (M): R$ ${m.toFixed(2)}`);
