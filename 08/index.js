b2a07c0cd978259c84e6aea7d438b473959359ee
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-05 14:46:27 -0300

    Alteração do nome das variáveis de acordo com o README

--- 08/index.js
+++ 08/index.js
@@ -1,8 +1,8 @@
 console.log('--- Cálculo do Perímetro e Área de um Círculo ---');
 console.log('Perímetro: C = 2*pi*R');
 console.log('Área:      A = pi*R²');
-let raio = 1;
-let perímetro = 2 * Math.PI * raio;
-let area = Math.PI * raio ** 2;
-console.log(`\nDados: \nRaio (R): ${raio}cm`);
-console.log(`\nResultado: \nPerímetro (C): ${perímetro.toFixed(1)}cm \nÁrea (A): ${area.toFixed(2)}cm²`);
+let r = 1;
+let c = 2 * Math.PI * r;
+let a = Math.PI * r ** 2;
+console.log(`\nDados: \nRaio (R): ${r}cm`);
+console.log(`\nResultado: \nPerímetro (C): ${c.toFixed(1)}cm \nÁrea (A): ${a.toFixed(2)}cm²`);
