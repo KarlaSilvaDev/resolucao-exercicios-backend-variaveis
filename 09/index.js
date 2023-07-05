16abeb2e0bbdb884e4d0747a0b2cb4f5ee2eb29a
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-05 14:47:37 -0300

    Alteração do nome das variáveis de acordo com o README

--- 09/index.js
+++ 09/index.js
@@ -1,9 +1,9 @@
 console.log('---Soma dos Ângulos Internos de um Polígono---');
 console.log('S = (n - 2)*180');
 console.log('Caso todos os ângulos sejam iguais: \na = S/n');
-let numLados= 6;
-let somaAngulosInternos = (numLados - 2)*180;
-let angulosIguais = somaAngulosInternos/numLados;
-console.log(`\nDados: \nNúmero de lados: ${numLados}`);
-console.log(`\nResultado: \nSoma dos ângulos internos (S): ${somaAngulosInternos}° \nValor de cada ângulo se todos forem iguais (a): ${angulosIguais}°`);
+let n= 6;
+let s = (n - 2)*180;
+let a = s/n;
+console.log(`\nDados: \nNúmero de lados: ${n}`);
+console.log(`\nResultado: \nSoma dos ângulos internos (S): ${s}° \nValor de cada ângulo se todos forem iguais (a): ${a}°`);
 
