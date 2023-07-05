6af0a00231f86d95ddfb3e4a0e9a72d218cf00dc
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-07-05 14:49:21 -0300

    Alteração do nome das variáveis de acordo com o README

--- 11/index.js
+++ 11/index.js
@@ -1,8 +1,8 @@
 console.log('--- Volume de uma Esfera ---');
 console.log('v = (4/3)* pi * r³');
-let diametro = 6;
-let volume = (4/3) * (diametro/2) ** 3;
-console.log(`\nDado: \nDiâmetro (d = 2r): ${diametro}cm`);
-console.log(`\nResultado: \nO volume de uma esfera de raio ${diametro/2}cm é igual a ${volume}PI cm³ `)
+let d = 6;
+let v = (4/3) * (d/2) ** 3;
+console.log(`\nDado: \nDiâmetro (d = 2r): ${d}cm`);
+console.log(`\nResultado: \nO volume de uma esfera de raio ${d/2}cm é igual a ${v}PI cm³ `)
 
 
